fb202c16cd1416a0c3530285b2d82f701fdfc821
Chen Xu <xuchen@thoughtworks.com>
2019-10-27 22:34:31 +0800

    add key vault cli

---
+++ keyVault/keyvault.ps1
@@ -0,0 +1,14 @@
+# To list locations: az account list-locations --output table
+
+$groupName = "keyvault"
+$keyVaultName = "ktomKeyVault"
+
+az group create --name $groupName --location "West US"
+
+az keyvault create --name $keyVaultName --resource-group $groupName --location "West US"
+
+az keyvault secret set --vault-name $keyVaultName --name "AppSecret" --value "MySecret"
+
+az keyvault secret show --name "AppSecret" --vault-name $keyVaultName
+
+
